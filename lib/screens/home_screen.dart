1d02b3c2daae2901e1df6fa50ca7975d9d27c054
Niraj-Node <nirajaghera999@gmail.com>
2024-09-25 01:31:09 +0530

    feat(permission): implement storage permission handling in HomeScreen
    - Added permission request logic in HomeScreen to ensure access to user photos.
    - Integrated dialog prompts for users when permission is denied, guiding them to settings if necessary.
    - Included the exit functionality to close the app when users cancel the permission request.
    - Updated AndroidManifest.xml to declare required permissions for storage access:
        - READ_EXTERNAL_STORAGE
        - WRITE_EXTERNAL_STORAGE
        - READ_MEDIA_IMAGES
        - READ_MEDIA_VIDEO

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,86 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:photo_manager/photo_manager.dart';
+import 'package:photoarc/utils/routes.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: const Center(
-        child: Text(''),
-      ),
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+// WidgetsBindingObserver is Interface for classes that register with the Widgets layer binding.
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _requestPermission(); // Request permission when the page loads
+  }
+
+  Future<void> _requestPermission() async {
+    // If permission is not granted, PhotoManager.requestPermissionExtend() prompts the user to grant permission.
+    // If permission is already granted, PhotoManager.requestPermissionExtend() returns the current permission status without prompting the user again.
+    PermissionState storagePermission = await PhotoManager.requestPermissionExtend();
+
+    if (storagePermission.isAuth) {
+      // If permission is granted, the addPostFrameCallback ensures that the navigation
+      // only happens after the current widget is fully rendered and displayed on the screen.
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        Navigator.of(context).pushReplacementNamed(Routes.gallery);
+      });
+    } else {
+      // Show a dialog or Snackbar if permission is denied
+      _showPermissionDeniedDialog();
+    }
+  }
+
+  void _showPermissionDeniedDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      // Disable dismissing the dialog by tapping outside
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text(
+            'Access Required',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          content: const Text(
+              'To access and display your photos, we need permission to your storage. Without this, the app will not function as intended.\n\n'
+                  'Would you like to grant access or open settings to change the permissions?'
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                // Close the app on cancel
+                exit(0);
+              },
+              child: const Text(
+                'Cancel',
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                // Open device settings for the app if the user chooses
+                Navigator.of(context).pop();
+                PhotoManager.openSetting();
+                Future.delayed(const Duration(milliseconds: 100), () {
+                  exit(0);
+                });
+              },
+              child: const Text('Open Settings'),
+            ),
+          ],
+        );
+      },
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // No UI is rendered as everything is permission-based navigation
+    return const SizedBox.shrink();
   }
 }
--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -2,17 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:photoarc/screens/home_screen.dart';
 import 'package:photoarc/screens/signin_screen.dart';
 import 'package:photoarc/screens/signup_screen.dart';
+import 'package:photoarc/screens/gallery_screen.dart';
 
 class Routes {
   static const String home = '/home';
   static const String signin = '/signin';
   static const String signup = '/signup';
+  static const String gallery = '/gallery';
 
   static Map<String, WidgetBuilder> getRoutes() {
     return {
       home: (context) => const HomeScreen(),
       signin: (context) => const SignInScreen(),
       signup: (context) => const SignUpScreen(),
+      gallery: (context) => const GalleryScreen(),
     };
   }
 }
